ab91d9875aed3688cc2f914996b45941afea2c19
Marcus Ortense <marcus.ortense@outlook.com>
2024-10-22 14:49:35 -0300

    chore(alpha): change build target from iife to esm

--- examples/minimal/tsup.config.js
+++ examples/minimal/tsup.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["./src/main.ts"],
-	clean: true,
-	format: ["iife"],
-	dts: false,
-	outDir: "./dist",
-	minify: false,
-	sourcemap: false,
+  entry: ["./src/main.ts"],
+  clean: true,
+  format: ["esm"],
+  dts: false,
+  outDir: "./dist",
+  minify: true,
+  sourcemap: false,
+  outExtension: ({ options }) => ({
+    js: options.minify ? ".min.js" : ".js"
+  })
 });
