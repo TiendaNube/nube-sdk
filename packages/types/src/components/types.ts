c5900c2b51051e16de5691fb4c0595b4625b85e2
Federico Dangelo <federico.dangelo@tiendanube.com>
2024-12-18 11:56:28 -0300

    Added __internalId to NubeComponent

--- packages/types/src/components/types.ts
+++ packages/types/src/components/types.ts
@@ -2,6 +2,8 @@ export type NubeComponentId = string;
 
 export type NubeComponentProps = {
 	id?: NubeComponentId;
+	// DON'T USE THIS, USED INTERNALLY BY THE SDK, ANY VALUE PASSED HERE WILL BE OVERWRITTEN
+	__internalId?: NubeComponentId;
 };
 
 export type ChildrenProps = {
