57f4c390e64176ba41ac0e04df4ca229f0fbf324
Marcus Ortense <marcus.ortense@outlook.com>
2025-01-23 14:58:51 -0300

    feat: create jsx ui package (#7)
    This PR adds the a new package to develop UI components with JSX
    * chore: update deps
    * chore: bump example dependencies version

--- examples/minimal-ui-jsx/src/main.tsx
+++ examples/minimal-ui-jsx/src/main.tsx
@@ -1,25 +1,28 @@
 import type { NubeSDK } from "@tiendanube/nube-sdk-types";
+import { Box, Field, Txt } from "@tiendanube/nube-sdk-jsx";
+
+function MyComponent() {
+  return (
+    <Box width={100} height={200}>
+      <Txt>Hello!!</Txt>
+      <Field
+        id="myField"
+        label="Name"
+        name="Name"
+        onChange={(e) => {
+          console.log(`User name: ${e.value}`);
+        }}
+      />
+    </Box>
+  );
+}
 
 export function App(nube: NubeSDK) {
-	nube.on("cart:update", ({ cart }) => {
-		console.log(cart);
-	});
-
-	nube.send("ui:slot:set", () => ({
-		ui: {
-			slots: {
-				after_line_items:
-					<box width={100} height={200}>
-						<field
-							id="myField"
-							label="Name"
-							name="Name"
-							onChange={(e) => {
-								console.log("User name: " + e.value)
-							}}
-						/>
-					</box>
-			}
-		}
-	}));
+  nube.send("ui:slot:set", () => ({
+    ui: {
+      slots: {
+        after_line_items: <MyComponent />,
+      },
+    },
+  }));
 }
