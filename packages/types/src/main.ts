9c5ece023ef629acdddba8a85b2fa5214d796454
Federico Dangelo <federico.dangelo@tiendanube.com>
2025-01-24 10:03:46 -0300

    Added localstorage definition to nube-sdk-types

--- packages/types/src/main.ts
+++ packages/types/src/main.ts
@@ -31,11 +31,25 @@ export type NubeSDKStateModifier = (
 	state: Readonly<NubeSDKState>,
 ) => DeepPartial<NubeSDKState>;
 
+export interface NubeLocalStorage {
+	// Returns the value associated with the given key, or null if the key does not exist in the storage.
+	getItemAsync(key: string): Promise<string | null>;
+
+	// Sets the value associated with the given key, with an optional expiration date.
+	// If no expiration date is specified, then the value will be stored indefinitely.
+	// The expiration date is in number of seconds since the UNIX epoch.
+	setItem(key: string, value: string, expirationDate?: number): void;
+	
+	// Removes the value associated with the given key from the storage.
+	removeItem(key: string): void;
+}
+
 export type NubeSDK = {
 	on(event: NubeSDKListenableEvent, listener: NubeSDKListener): void;
 	off(event: NubeSDKListenableEvent, listener: NubeSDKListener): void;
 	send(event: NubeSDKSendableEvent, modifier?: NubeSDKStateModifier): void;
 	getState(): Readonly<NubeSDKState>;
+	getLocalStorage(): NubeLocalStorage;
 };
 
 export type NubeApp = (nube: NubeSDK) => void;
