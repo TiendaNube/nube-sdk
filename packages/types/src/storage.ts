5d7e1e5ec1286c9d21fd7624d0576aaba58edc6b
Federico Dangelo <federico.dangelo@tiendanube.com>
2025-02-14 09:45:34 -0300

    Fixed format

--- packages/types/src/storage.ts
+++ packages/types/src/storage.ts
@@ -49,6 +49,7 @@ export type NubeStorageQueryResponseEventData =
 export type NubeStorageEventData =
 	| NubeStorageQueryEventData
 	| NubeStorageQueryResponseEventData;
+
 export interface AsyncNubeStorage {
 	// Returns the value associated with the given key, or null if the key does not exist in the storage.
 	getItem(key: string): Promise<string | null>;
