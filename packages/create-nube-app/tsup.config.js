2d6e01b0931e887d2a22898a44ad667bf84679e5
Rodrigo Lourenço <rsilvape@gmail.com>
2025-03-21 14:14:16 -0300

    config tsup bundle

--- packages/create-nube-app/tsup.config.js
+++ packages/create-nube-app/tsup.config.js
@@ -2,12 +2,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'], // ponto de entrada do seu projeto
-  format: ['esm'],  // formatos de saída
-  bundle: false,           // não agrupa os módulos em um único arquivo
-  minify: false,           // desabilita a minificação
-  sourcemap: false,         // gera sourcemaps para facilitar o debug
-  clean: true,             // limpa a pasta de saída antes de cada build
-  target: 'node16',        // define o alvo (ajuste conforme sua versão do Node)
-  dts: false,               // gera arquivos de definição TypeScript (.d.ts)
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  bundle: false,
+  minify: false,
+  sourcemap: false,
+  clean: true,
+  target: 'node16',
+  dts: false,
 });
