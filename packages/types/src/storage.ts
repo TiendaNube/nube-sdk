a7e6bb0b4b9cd68b65cc0230a1a2c082300a6ba1
Rodrigo Lourenço <rsilvape@gmail.com>
2025-03-28 11:46:55 -0300

    refactor(types): rename 'expirationDate' to 'ttl' in NubeStorageSetEventData

--- packages/types/src/storage.ts
+++ packages/types/src/storage.ts
@@ -25,7 +25,7 @@ export type NubeStorageGetResponseEventData = NubeStorageEventDataBase & {
 export type NubeStorageSetEventData = NubeStorageEventDataBase & {
 	key: string;
 	value: string;
-	expirationDate?: number;
+	ttl?: number;
 };
 
 export type NubeStorageSetResponseEventData = NubeStorageEventDataBase;
@@ -59,12 +59,12 @@ export interface AsyncNubeStorage {
 
 	/**
 	 * Sets the value associated with the given key, with an optional expiration date.
-	 * If no expiration date is specified, then the value will be stored indefinitely.
+	 * If no ttl is specified, then the value will be stored indefinitely.
 	 * @param key Key to store the value for.
 	 * @param value Value to store.
-	 * @param expirationDate Optional expiration date, in number of seconds since the UNIX epoch.
+	 * @param ttl Time-to-live (TTL) in seconds, specifying how long the value should be stored.
 	 */
-	setItem(key: string, value: string, expirationDate?: number): Promise<void>;
+	setItem(key: string, value: string, ttl?: number): Promise<void>;
 
 	/**
 	 * Removes the value associated with the given key from the storage.
