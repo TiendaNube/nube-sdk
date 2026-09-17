dfddb6487ed7ff9d552ba6806bbfef13b43ca278
Filipe Badaró <badaro@gmail.com>
2026-09-17 11:50:36 -0300

    feat(slots) Add institutional message slots (#493)

--- packages/types/src/slots.ts
+++ packages/types/src/slots.ts
@@ -175,6 +175,8 @@ export const CHECKOUT_UI_SLOT = {
  * @property {"after_section_testimonials"} AFTER_SECTION_TESTIMONIALS - After the testimonials section on the home page.
  * @property {"before_section_welcome"} BEFORE_SECTION_WELCOME - Before the welcome message section on the home page.
  * @property {"after_section_welcome"} AFTER_SECTION_WELCOME - After the welcome message section on the home page.
+ * @property {"before_section_institutional"} BEFORE_SECTION_INSTITUTIONAL - Before the institutional message section on the home page.
+ * @property {"after_section_institutional"} AFTER_SECTION_INSTITUTIONAL - After the institutional message section on the home page.
  * @property {"before_product_detail_related_products"} BEFORE_PRODUCT_DETAIL_RELATED_PRODUCTS - Before the related products section on the product detail page.
  * @property {"after_product_detail_related_products"} AFTER_PRODUCT_DETAIL_RELATED_PRODUCTS - After the related products section on the product detail page.
  * @property {"before_product_detail_complementary_products"} BEFORE_PRODUCT_DETAIL_COMPLEMENTARY_PRODUCTS - Before the complementary products section on the product detail page.
@@ -277,6 +279,8 @@ export const STOREFRONT_UI_SLOT = {
 	AFTER_SECTION_TESTIMONIALS: "after_section_testimonials",
 	BEFORE_SECTION_WELCOME: "before_section_welcome",
 	AFTER_SECTION_WELCOME: "after_section_welcome",
+	BEFORE_SECTION_INSTITUTIONAL: "before_section_institutional",
+	AFTER_SECTION_INSTITUTIONAL: "after_section_institutional",
 	BEFORE_PRODUCT_DETAIL_RELATED_PRODUCTS:
 		"before_product_detail_related_products",
 	AFTER_PRODUCT_DETAIL_RELATED_PRODUCTS:
